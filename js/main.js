1d243d81294099c64017c29ee96141a1d8365672
Alberto <ramirez-alberto@zoho.com>
2022-02-05 17:20:09 -0800

    Add a function to showResults in the DOM

--- js/main.js
+++ js/main.js
@@ -1,65 +1,78 @@
-const gameOptions = ['ROCK', 'PAPER', 'SCISSORS'];
-/* 
-function game() {
-    let playerScore = 0, computerScore = 0, playerWin;
 
 
-    for (let i = 1; i < 6; i++) {
-        const computerSelection = computerPlay();
-        const playerSelection = prompt("Choose ROCK,PAPER or SCISSORS:", '').toUpperCase();
-        if (gameOptions.indexOf(playerSelection) > -1) {
-            playerWin = playRound(playerSelection, computerSelection);
+function game(playerSelection, roundNumber, playerScore) {
+    const gameOptions = ['ROCK', 'PAPER', 'SCISSORS'];
+    let  playerRound;
 
-            if (playerWin === 1) {
-                playerScore++;
-                console.log(`Round ${i}: You WIN! ${playerSelection} beats ${computerSelection}`);
-            } else {
-                computerScore++;
-                console.log(`Round ${i}: You Lose! Player: ${playerSelection} Computer: ${computerSelection}`);
-            }
-
-        } else {
-            console.log(`Round ${i}: Try again, ${playerSelection} is not a valid option.`);
-        }
-    }
-    console.log(`Final Score: \n Player:${playerScore} \n Computer: ${computerScore}`);
-}
- */
-
-function computerPlay() {
-
-    const chooseRandomOption = gameOptions[Math.floor(Math.random() * gameOptions.length)];
-    return chooseRandomOption;
-}
-
-function playRound(playerSelection, computerSelection) {
-    let winner = 0;
+    const computerSelection = computerPlay();
     playerSelection = playerSelection.toUpperCase();
-    //Rock beats Scissors
-    if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
-        winner++;
-    }
-    //Paper beats Rock
-    else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-        winner++;
-    }
-    //Scissors beat Paper
-    else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        winner++;
+
+    const checkPlayerSelectionExists = gameOptions.indexOf(playerSelection) > -1;
+    if (checkPlayerSelectionExists) {
+        playerRound = playRound(playerSelection, computerSelection);
+
+        if (playerRound === 1) {
+            
+            showResults(`Round ${roundNumber}: You WIN! ${playerSelection} beats ${computerSelection}`);
+            return 1;
+        } else {
+            showResults(`Round ${roundNumber}: You Lose! Player: ${playerSelection} Computer: ${computerSelection}`);
+            return 0;
+        }
+
+    } else {
+        showResults(`Round ${roundNumber}: Try again, ${playerSelection} is not a valid option.`);
     }
 
-    return winner;
+
+    function computerPlay() {
+
+        const chooseRandomOption = gameOptions[Math.floor(Math.random() * gameOptions.length)];
+        return chooseRandomOption;
+    }
+
+    function playRound(playerSelection, computerSelection) {
+        let winner = 0;
+        //Rock beats Scissors
+        if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+            winner++;
+        }
+        //Paper beats Rock
+        else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+            winner++;
+        }
+        //Scissors beat Paper
+        else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+            winner++;
+        }
+
+        return winner;
+    }
+
+
 }
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) =>{
-    button.addEventListener('click',(e) => {
-        let roundResult = playRound(e.target.id,computerPlay());
-        showResults(roundResult);
-    });
-});
 
-function showResults(result){
+
+let roundNumber = 1, playerScore = 0;
+function showResults(result) {
     const resulltBoard = document.querySelector('#resultBoard');
     resulltBoard.textContent = result;
 }
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {       
+        playerScore += game(e.target.id, roundNumber, playerScore);
+        console.log(playerScore);
+        if (roundNumber === 5) {
+            showResults(`Final Score: \n Player:${playerScore} \n Computer: ${roundNumber - playerScore}`);
+            roundNumber = 1;
+            playerScore = 0;
+        }else{
+        
+        roundNumber++;}
+       
+
+    });
+});
